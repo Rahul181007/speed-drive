41e588c0c8bbfe4b688920c26a20da62cfe8a9d6
Rahul181007 <rahulsheeraj@gmail.com>
2026-05-08 12:39:46 +0530

    test 2

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import API from "../services/api";
 import logo from "../assets/logo.jpeg";
-import axios from "axios";
+
 const Signup = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
