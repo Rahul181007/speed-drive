36bfa68d2093a0a096e2b9db2ad9fd5400974310
Rahul181007 <rahulsheeraj@gmail.com>
2026-05-08 12:38:50 +0530

    test

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -27,13 +27,27 @@ const Signup = () => {
             navigate("/", { replace: true });
         }
         catch (error: unknown) {
-        if (axios.isAxiosError(error)) {
-            setErrorMsg(
-                error.response?.data?.message || "Something went wrong"
-            );
-        } else {
-            setErrorMsg("Something went wrong");
-        }
+            if (
+                typeof error === "object" &&
+                error !== null &&
+                "response" in error
+            ) {
+
+                const axiosError = error as {
+                    response?: {
+                        data?: {
+                            message?: string;
+                        };
+                    };
+                };
+
+                setErrorMsg(
+                    axiosError.response?.data?.message || "Something went wrong"
+                );
+
+            } else {
+                setErrorMsg("Something went wrong");
+            }
             if (import.meta.env.DEV) {
                 console.error(error);
             }
@@ -41,105 +55,105 @@ const Signup = () => {
     };
 
     useEffect(() => {
-    const token = localStorage.getItem("token");
+        const token = localStorage.getItem("token");
 
-    if (token) {
-        navigate("/dashboard", { replace: true });
-    }
-}, [navigate]);
+        if (token) {
+            navigate("/dashboard", { replace: true });
+        }
+    }, [navigate]);
 
     return (
-    <div className="flex min-h-screen items-center justify-center bg-linear-to-r from-blue-300 to-green-300">
-        
-        <div className="w-96 rounded-2xl bg-white p-8 shadow-lg">
+        <div className="flex min-h-screen items-center justify-center bg-linear-to-r from-blue-300 to-green-300">
 
-            
-            <div className="flex items-end justify-center gap-1 mb-4">
-                <img src={logo} alt="logo" className="h-12 w-auto" />
-                <span className="text-sm font-semibold text-gray-900 mb-1">
-                    Speedo
-                </span>
-            </div>
+            <div className="w-96 rounded-2xl bg-white p-8 shadow-lg">
 
-            {/* TITLE (optional subtle) */}
-            <h2 className="mb-4 text-center text-base text-gray-600">
-                Create your account
-            </h2>
 
-            {/* ERROR */}
-            {errorMsg && (
-                <div className="mb-4 text-sm text-red-500 bg-red-50 p-2 rounded">
-                    {errorMsg}
+                <div className="flex items-end justify-center gap-1 mb-4">
+                    <img src={logo} alt="logo" className="h-12 w-auto" />
+                    <span className="text-sm font-semibold text-gray-900 mb-1">
+                        Speedo
+                    </span>
                 </div>
-            )}
 
-            {/* NAME */}
-            <div className="mb-4">
-                <label className="text-sm text-gray-600">Name</label>
-                <input
-                    type="text"
-                    placeholder="Your name"
-                    className="w-full mt-1 border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                    value={name}
-                    onChange={(e) => {
-                        setName(e.target.value);
-                        setErrorMsg("");
-                    }}
-                />
-            </div>
+                {/* TITLE (optional subtle) */}
+                <h2 className="mb-4 text-center text-base text-gray-600">
+                    Create your account
+                </h2>
 
-            {/* EMAIL */}
-            <div className="mb-4">
-                <label className="text-sm text-gray-600">Email</label>
-                <input
-                    type="email"
-                    placeholder="example@email.com"
-                    className="w-full mt-1 border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                    value={email}
-                    onChange={(e) => {
-                        setEmail(e.target.value);
-                        setErrorMsg("");
-                    }}
-                />
-            </div>
+                {/* ERROR */}
+                {errorMsg && (
+                    <div className="mb-4 text-sm text-red-500 bg-red-50 p-2 rounded">
+                        {errorMsg}
+                    </div>
+                )}
 
-            {/* PASSWORD */}
-            <div className="mb-6">
-                <label className="text-sm text-gray-600">Password</label>
-                <input
-                    type="password"
-                    placeholder="At least 6 characters"
-                    className="w-full mt-1 border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                    value={password}
-                    onChange={(e) => {
-                        setPassword(e.target.value);
-                        setErrorMsg("");
-                    }}
-                />
-            </div>
+                {/* NAME */}
+                <div className="mb-4">
+                    <label className="text-sm text-gray-600">Name</label>
+                    <input
+                        type="text"
+                        placeholder="Your name"
+                        className="w-full mt-1 border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                        value={name}
+                        onChange={(e) => {
+                            setName(e.target.value);
+                            setErrorMsg("");
+                        }}
+                    />
+                </div>
 
-            {/* BUTTON */}
-            <button
-                onClick={handleSignup}
-                className="w-full bg-black hover:bg-gray-800 transition text-white py-2 rounded-md"
-            >
-                Sign up
-            </button>
+                {/* EMAIL */}
+                <div className="mb-4">
+                    <label className="text-sm text-gray-600">Email</label>
+                    <input
+                        type="email"
+                        placeholder="example@email.com"
+                        className="w-full mt-1 border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                        value={email}
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                            setErrorMsg("");
+                        }}
+                    />
+                </div>
 
-            {/* LOGIN LINK */}
-            <p className="mt-4 text-center text-sm">
-                Already have an account?{" "}
-                <span
-                    className="text-blue-600 cursor-pointer hover:underline"
-                    onClick={() => navigate("/")}
+                {/* PASSWORD */}
+                <div className="mb-6">
+                    <label className="text-sm text-gray-600">Password</label>
+                    <input
+                        type="password"
+                        placeholder="At least 6 characters"
+                        className="w-full mt-1 border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                        value={password}
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                            setErrorMsg("");
+                        }}
+                    />
+                </div>
+
+                {/* BUTTON */}
+                <button
+                    onClick={handleSignup}
+                    className="w-full bg-black hover:bg-gray-800 transition text-white py-2 rounded-md"
                 >
-                    Login
-                </span>
-            </p>
+                    Sign up
+                </button>
 
+                {/* LOGIN LINK */}
+                <p className="mt-4 text-center text-sm">
+                    Already have an account?{" "}
+                    <span
+                        className="text-blue-600 cursor-pointer hover:underline"
+                        onClick={() => navigate("/")}
+                    >
+                        Login
+                    </span>
+                </p>
+
+            </div>
         </div>
-    </div>
-);
+    );
 };
 
 export default Signup;
